8fd2bebc1aa13753d6f714d87262e42b4cdab25b
pedrotp <pedro@pedrotp.com>
2018-02-05 21:47:30 -0500

    fixed bug that prevented compilation

--- projects/torresmp/dominion/dominion.c
+++ projects/torresmp/dominion/dominion.c
@@ -670,7 +670,7 @@ int adventurerCard(struct gameState *state, int* temphand) {
     }
     drawCard(currentPlayer, state);
     cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1]; //top card of hand is most recently drawn card.
-    if (cardDrawn == copper || cardDrawn = silver || cardDrawn == gold) {
+    if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold) {
       drawntreasure++;
     }
     else {
